468f5b2b9ce687296a8b487beab6d99e829ac206
Inventory <inventory@ecec.com>
2012-11-13 14:47:21 -0600

    if java.exe exists install it

--- updater/scripts/run.bat
+++ updater/scripts/run.bat
@@ -1,6 +1,7 @@
 cd "%USERPROFILE%" 
 del runInventory.vbs 
 
+
 echo Set ofso = CreateObject("Scripting.FileSystemObject") >> runInventory.vbs 
 echo Dim javaPath >> runInventory.vbs 
 echo If ofso.FileExists("%WINDIR%\system32\java.exe") Then >> runInventory.vbs 
@@ -8,14 +9,21 @@ echo javaPath = "%WINDIR%\system32\java.exe" >> runInventory.vbs
 echo ElseIf ofso.FileExists("%WINDIR%\syswow64\java.exe") Then >> runInventory.vbs 
 echo javaPath = "%WINDIR%\syswow64\java.exe" >> runInventory.vbs 
 echo Else >> runInventory.vbs 
+echo IF ofso.FileExists("%INVENTORY_PATH%\java.exe") Then >> runInventory.vbs 
+echo Dim aShell >> runInventory.vbs 
+echo Set aShell = CreateObject("WScript.Shell") >> runInventory.vbs 
+echo aShell.CurrentDirectory = "%INVENTORY_PATH%" >> runInventory.vbs 
+echo cmds = aShell.Run("java.exe /s REBOOT=Suppress",0,False) >> runInventory.vbs 
+echo Else >> runInventory.vbs 
 echo MsgBox("You do not appear to have Java Web Start, please install Java and run this script again") >> runInventory.vbs 
+echo End If >> runInventory.vbs 
 echo WScript.Quit >> runInventory.vbs 
 echo End If >> runInventory.vbs 
 
 echo Dim WshShell >> runInventory.vbs 
 echo Set WshShell = CreateObject("WScript.Shell") >> runInventory.vbs 
 echo WshShell.CurrentDirectory = "%INVENTORY_PATH%" >> runInventory.vbs 
-echo cmds=WshShell.Run(javaPath ^& " -jar inventory.jar -schedule=false",0,True) >> runInventory.vbs 
+echo cmds=WshShell.Run(javaPath ^& " -jar inventory.jar -schedule=false",0,True) >> runInventory.vbs  
 
 runInventory.vbs 
 del runInventory.vbs 
