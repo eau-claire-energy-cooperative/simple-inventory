302ca605ce2e465c0d4836d6ba8f06d6f7a58ef6
robweber <robweberjr@gmail.com>
2014-01-09 08:45:41 -0600

    added Linux commands

---
+++ updater/src/com/ecec/rweber/inventory/commands/OSLinuxCommand.java
@@ -0,0 +1,24 @@
+package com.ecec.rweber.inventory.commands;
+
+import org.hyperic.sigar.OperatingSystem;
+import org.hyperic.sigar.Sigar;
+import com.ecec.rweber.inventory.utils.PCInfo;
+
+public class OSLinuxCommand implements SigarCommand{
+
+	@Override
+	public PCInfo runCommand(Sigar sigar, PCInfo currentInfo) {
+		
+		OperatingSystem os = OperatingSystem.getInstance();
+		
+		currentInfo.addField("OS",os.getDescription());
+		currentInfo.addField("OS_Arch", os.getArch());
+		currentInfo.addField("OS_Version", os.getVersion());
+		currentInfo.addField("OS_Patch_Level",os.getPatchLevel());
+		currentInfo.addField("OS_Vendor", os.getVendor());
+		currentInfo.addField("OS_Code_Name", os.getVendorCodeName());
+		
+		return currentInfo;
+	}
+
+}
