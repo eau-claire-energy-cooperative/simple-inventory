2a076dd04f9b9e31e7dffae6f57f579155e245c9
robweber <robweberjr@gmail.com>
2014-12-15 10:54:47 -0600

    this does nothing right now

--- www/app/Console/Command/Task/ShutdownComputerTask.php
+++ www/app/Console/Command/Task/ShutdownComputerTask.php
@@ -1,37 +1,9 @@
 <?php
-class WakeComputerTask extends AppShell {
+class ShutdownComputerTask extends AppShell {
     public $uses = array('Computer');
 
     public function execute($params) {
-    	App::uses("PingComponent","Controller/Component");
-    	App::uses('ComponentCollection', 'Controller');
     	
-    	$collection = new ComponentCollection();
-    	$pingComp = new PingComponent($collection);
-    	
-    	//check if computer is awake
-    	$replies = $pingComp->ping($params['Computer Name']);
-    	
-    	if($replies['transmitted'] == $replies['received'])
-    	{
-    		//check if this is a shutdown or a restart
-    		if($params['Restart'] == 'true')
-    		{
-    			
-    		}
-    		//log
-    		$this->out(" " . $params['Computer Name']);
-    		$this->log("Waking up " . $params['Computer Name']);
-    	
-    		//get the computer MAC
-    		$computer = $this->Computer->find('first',array('conditions'=>array("Computer.ComputerName"=>$params['Computer Name'])));
-    	
-    		$pingComp->wol('10.10.10.35',$computer['Computer']['MACaddress']);
-    	}
-    	else
-    	{
-    		$this->out("Computer " . $params['Computer Name'] . " is already awake");
-    	}
     }
 }
 ?>
