1520fee812a9fc215c077507230f587392143975
robweber <robweberjr@gmail.com>
2014-12-15 08:25:36 -0600

    renamed to make consistant with other naming

---
+++ www/app/Model/Logs.php
@@ -0,0 +1,7 @@
+<?php
+   class Logs extends AppModel {
+   			
+   	var $useTable = 'LOGS';
+	
+	//var $hasMany = array('Computer', array('className' =>'Computer', 'foreignKey' => 'id', 'fields' => 'ComputerName'));
+}
