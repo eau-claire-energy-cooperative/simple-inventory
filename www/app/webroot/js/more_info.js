1668cdc90174f3320bea86c92b1af19406a77878
Rob Weber <rweber@ecec.com>
2020-03-04 11:06:58 -0600

    remove old fancybox loading code

--- www/app/webroot/js/more_info.js
+++ www/app/webroot/js/more_info.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
 	checkRunning();
 	setInterval(checkRunning,40 * 1000);
-	
-	$(".popup").fancybox({
-	maxWidth	: 600,
-	maxHeight	: 400,
-	fitToView	: false,
-	width		: '70%',
-	height		: '70%',
-	autoSize	: false,
-	closeClick	: false,
-	openEffect	: 'none',
-	closeEffect	: 'none'
-	});
 });
