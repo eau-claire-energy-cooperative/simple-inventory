21243393ed4738d4508cdacaa1627ba305d44efb
Rob Weber <rweber@ecec.com>
2020-03-18 13:26:38 -0500

    always set active menu option if it doesn't exist

--- www/app/Controller/AppController.php
+++ www/app/Controller/AppController.php
@@ -32,5 +32,10 @@ App::uses('Controller', 'Controller');
  * @link http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-	
+    public function beforeRender(){
+        if(!isset($this->viewVars['active_menu']))
+        {
+            $this->set('active_menu','');
+        }
+    }
 }
