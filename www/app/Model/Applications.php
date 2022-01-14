4426ce250bb6774cfa7fe0c536b0340901844a12
Rob Weber <rweber@ecec.com>
2022-01-14 14:35:17 -0600

    added a virtual field to concat name and version number

--- www/app/Model/Applications.php
+++ www/app/Model/Applications.php
@@ -2,6 +2,7 @@
    class Applications extends AppModel {
 
    	var $useTable = 'applications';
+    var $virtualFields = array('full_name'=>"CONCAT(Applications.name,' ',Applications.version)");
 
     var $hasAndBelongsToMany = array(
       "Computer" => array('className'=>"Computer",
