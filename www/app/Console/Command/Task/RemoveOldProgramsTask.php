efad787589497a05791381b5fc889215599b9db0
Rob Weber <rweber@ecec.com>
2022-01-20 08:37:13 -0600

    don't delete applications when monitoring is enabled

--- www/app/Console/Command/Task/RemoveOldProgramsTask.php
+++ www/app/Console/Command/Task/RemoveOldProgramsTask.php
@@ -12,8 +12,8 @@ class RemoveOldProgramsTask extends AppShell {
       $total_deleted = 0;
       foreach($applications as $app)
       {
-        //delete if no devices are currently assigned and lifecycle does not exist
-        if(count($app['Computer']) == 0 && $app['Lifecycle']['id'] == NULL)
+        //delete if no devices are currently assigned, lifecycle does not exist, and no monitoring is configured
+        if(count($app['Computer']) == 0 && $app['Lifecycle']['id'] == NULL && $app['Applications']['monitoring'] != 'true')
         {
           $total_deleted ++;
           $this->out("No devices assigned to application: " . $app['Applications']['name']);
