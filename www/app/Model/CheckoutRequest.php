b219a4a2c4c6e9ee4d212b3048ca0e2893e9ba1c
Rob Weber <rweber@ecec.com>
2023-12-28 10:28:12 -0600

    added virtual field for unix timestamps

--- www/app/Model/CheckoutRequest.php
+++ www/app/Model/CheckoutRequest.php
@@ -2,6 +2,8 @@
 class CheckoutRequest extends AppModel {
 
 	var $useTable = 'checkout_request';
+  var $virtualFields = array('check_out_unix'=>"UNIX_TIMESTAMP(CheckoutRequest.check_out_date)",
+                             'check_in_unix'=>"UNIX_TIMESTAMP(CheckoutRequest.check_in_date)");
 
 	var $belongsTo = array("DeviceType" => array("foreignKey" => "device_type"));
   var $hasAndBelongsToMany = array(
