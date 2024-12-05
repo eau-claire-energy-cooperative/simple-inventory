44efd18317d84c0c510841d700f6313b90d71d6a
Rob Weber <rweber@ecec.com>
2024-12-05 15:38:30 -0600

    version bump

--- www/app/Config/bootstrap.php
+++ www/app/Config/bootstrap.php
@@ -112,7 +112,7 @@ CakeLog::config('error', array(
 Configure::write('Security.useOpenSsl', true);
 
 //the current program version
-Configure::write('Settings.version', '5.1');
+Configure::write('Settings.version', '6.0');
 
 //load the custom.ini config
 Configure::load('custom');
