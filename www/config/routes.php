407b1226b5de989c02cfe01d33905196bd8f771c
Rob Weber <rweber@ecec.com>
2025-01-02 15:57:39 -0600

    default route

--- www/config/routes.php
+++ www/config/routes.php
@@ -55,12 +55,7 @@ return function (RouteBuilder $routes): void {
          * its action called 'display', and we pass a param to select the view file
          * to use (in this case, templates/Pages/home.php)...
          */
-        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-
-        /*
-         * ...and connect the rest of 'Pages' controller's URLs.
-         */
-        $builder->connect('/pages/*', 'Pages::display');
+        $builder->connect('/', ['controller' => 'inventory', 'action' => 'computerInventory']);
 
         /*
          * Connect catchall routes for all controllers.
