74f163297f0e723a3c177a3dc8103efa56affd34
Rob Weber <rweber@ecec.com>
2025-01-23 15:43:39 -0600

    fix database timestamps

--- www/config/app.php
+++ www/config/app.php
@@ -300,7 +300,7 @@ return [
             'className' => Connection::class,
             'driver' => Mysql::class,
             'persistent' => false,
-            'timezone' => 'UTC',
+            //'timezone' => 'UTC',
 
             /*
              * For MariaDB/MySQL the internal default changed from utf8 to utf8mb4, aka full utf-8 support, in CakePHP 3.6
