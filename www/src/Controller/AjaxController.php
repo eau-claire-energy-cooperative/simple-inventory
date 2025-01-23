8a369d3c4ce1dee8533e63b5f324232bd6071138
Rob Weber <rweber@ecec.com>
2025-01-23 11:27:25 -0600

    make json response a generic template

--- www/src/Controller/AjaxController.php
+++ www/src/Controller/AjaxController.php
@@ -29,6 +29,50 @@ class AjaxController extends AppController {
     }
 
     $this->set('result', $isRunning);
+    $this->render('json');
+	}
+
+  function wol($host, $mac)
+	{
+		//get the broadcast addr
+		$range = $this->iprange($host,24);
+		$broadcast = $range['last_ip'];
+
+		$mac_array = explode(':', $mac);
+
+		$hwaddr = '';
+
+		foreach($mac_array AS $octet)
+		{
+			$hwaddr .= chr(hexdec($octet));
+		}
+
+		// Create Magic Packet
+
+		$packet = '';
+		for ($i = 1; $i <= 6; $i++)
+		{
+			$packet .= chr(255);
+		}
+
+		for ($i = 1; $i <= 16; $i++)
+		{
+			$packet .= $hwaddr;
+		}
+
+		$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+		if ($sock)
+		{
+			$options = socket_set_option($sock, SOL_SOCKET, SO_BROADCAST, true);
+
+			if ($options >=0)
+			{
+				$e = socket_sendto($sock, $packet, strlen($packet), 0, $broadcast, 9);
+				socket_close($sock);
+			}
+		}
+
+    $this->render('json');
 	}
 }
 ?>
