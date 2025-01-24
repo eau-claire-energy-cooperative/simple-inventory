c696fe89180c32e2c54f5b175f89b75d903084a4
Rob Weber <rweber@ecec.com>
2025-01-24 13:32:33 -0600

    allow for html with escape var

--- www/templates/element/flash/success.php
+++ www/templates/element/flash/success.php
@@ -1,3 +1,7 @@
 <div class="alert alert-success mt-2" role="alert">
-  <?php echo h($message) ?>
+  <?php if(array_key_exists('escape', $params) && !$params['escape']): ?>
+    <?= $message ?>
+  <?php else: ?>
+    <?php echo h($message) ?>
+  <?php endif; ?>
 </div>
