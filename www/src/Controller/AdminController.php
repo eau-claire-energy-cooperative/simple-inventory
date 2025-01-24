2f892544eddeccb7cd46a3ee941ee9b168c949e1
Rob Weber <rweber@ecec.com>
2025-01-24 15:10:37 -0600

    advanced settings editor

--- www/src/Controller/AdminController.php
+++ www/src/Controller/AdminController.php
@@ -30,6 +30,39 @@ class AdminController extends AppController {
 		$this->set('title','Downloads');
 	}
 
+  public function editSetting($id = null){
+		$this->set('title','Add Setting');
+
+    $Setting = $this->fetchTable('Setting');
+
+    if($this->request->is('get'))
+		{
+			if(isset($id))
+			{
+				//get the information about this id
+				$this->set('title','Edit Setting');
+				$this->set('setting', $Setting->get($id));
+			}
+      else{
+        $this->set('setting', $Setting->newEmptyEntity());
+      }
+		}
+		else
+		{
+      $setting = $Setting->newEntity($this->request->getData());
+
+			if ($Setting->save($setting)) {
+        $this->Flash->success('Setting saved');
+
+        return $this->redirect(array('action' => 'settings'));
+    	}
+    	else
+    	{
+        $this->Flash->error('Unable to update the setting');
+    	}
+		}
+	}
+
   function index(){
     $this->set('title', 'Admin');
   }
@@ -42,6 +75,23 @@ class AdminController extends AppController {
 	 	$this->set('logs',$this->paginate($logs));
 
 		$this->set('inventory', $this->fetchTable('Computer')->find('list', ['keyField'=>'ComputerName', 'valueField'=>'id'])->toArray());
+	}
+
+  public function settings2($delete = null){
+		$this->set('title', 'Advanced Settings');
+
+    $Setting = $this->fetchTable('Setting');
+		if(isset($delete))
+		{
+			//delete the id given
+      $setting = $Setting->get($this->request->getQuery('id'));
+			$Setting->delete($setting);
+
+			$this->Flash->success('Setting deleted');
+
+		}
+
+		$this->set('settings_list', $Setting->find('all', ['order'=>['Setting.key']])->all());
 	}
 }
 ?>
