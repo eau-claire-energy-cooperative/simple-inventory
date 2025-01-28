f0353edabada6f99c893920cc34254688697d8c4
Rob Weber <rweber@ecec.com>
2025-01-28 09:52:06 -0600

    add/edit/delete device types

--- www/src/Controller/ManageController.php
+++ www/src/Controller/ManageController.php
@@ -18,10 +18,67 @@ class ManageController extends AppController {
     $this->set("settings", $settings);
   }
 
+  public function addDeviceType() {
+    $this->set('title','Add Device Type');
+    $this->set('allowedAttributes', array_merge($this->DEVICE_ATTRIBUTES['GENERAL'], $this->DEVICE_ATTRIBUTES['HARDWARE'], $this->DEVICE_ATTRIBUTES['NETWORK']));
+
+    if ($this->request->is('post')) {
+      $DeviceType = $this->fetchTable('DeviceType');
+
+      $newDevice = $DeviceType->newEntity($this->request->getData());
+      $newDevice->attributes = implode(",", $this->request->getData('attributes'));
+
+      if ($DeviceType->save($newDevice)) {
+          $this->Flash->success(sprintf('%s has been saved', $newDevice['name']));
+          return $this->redirect(array('action' => 'deviceTypes'));
+      } else {
+          $this->Flash->error('Unable to add your entry');
+      }
+    }
+  }
+
+  public function deleteDeviceType($id) {
+    $DeviceType = $this->fetchTable('DeviceType');
+    $device = $DeviceType->get($id);
+
+    if ($DeviceType->delete($device)) {
+      $this->Flash->success(sprintf('%s has been deleted', $device['name']));
+      return $this->redirect(['action' => 'deviceTypes']);
+    }
+  }
+
   public function deviceTypes() {
     $this->set('title','Device Types');
     $this->set('device_types', $this->fetchTable('DeviceType')->find('all', ['contain'=>['Computer'],
                                                                              'order'=>['name'=>'ASC']])->all());
+  }
+
+  public function editDeviceType($id) {
+    $this->set('title','Edit Device Type');
+    $this->set('allowedAttributes', array_merge($this->DEVICE_ATTRIBUTES['GENERAL'], $this->DEVICE_ATTRIBUTES['HARDWARE'], $this->DEVICE_ATTRIBUTES['NETWORK']));
+
+    $DeviceType = $this->fetchTable('DeviceType');
+    $device = $DeviceType->get($id);
+    
+    if ($this->request->is('get')) {
+      $this->set('device', $device);
+    }
+    else
+    {
+      $DeviceType->patchEntity($device, $this->request->getData());
+      $device->attributes = implode(",", $this->request->getData('attributes'));
+
+      if ($DeviceType->save($device)) {
+        $this->Flash->success(sprintf('%s has been updated', $device['name']));
+        $this->redirect(array('action' => 'deviceTypes'));
+      }
+      else
+      {
+        $this->Flash->error(sprintf('Unable to update %s', $device['name']));
+      }
+
+      return $this->redirect('/admin/deviceTypes');
+    }
   }
 
 }
---
+++ www/templates/Manage/edit_device_type.php
@@ -0,0 +1,59 @@
+<script type="text/javascript">
+function updateIcon(){
+  $('#device_icon_preview').attr('class', 'mdi mdi-' + $('#DeviceTypeIcon').val());
+}
+</script>
+<?= $this->Form->create($device); ?>
+<?= $this->Form->input('id', array('type' => 'hidden'));?>
+
+<?php
+  //convert string to array
+  $assignedAttributes = explode(',', $device['attributes']);
+?>
+<div class="row">
+  <div class="col-xl-12">
+    <div class="card shadow mb-4">
+        <div class="card-body">
+          <p>To find a device type icon you can search the <a href="https://materialdesignicons.com/" target="_blank">Matrial Design Icons</a> site.</p>
+          <div class="row mb-2">
+            <div class="col-md-4">Device Type Name: </div>
+            <div class="col-md-8"><?= $this->Form->input('name', ['class'=>'form-control']);?></div>
+          </div>
+          <div class="row mb-2">
+            <div class="col-md-4">Icon: </div>
+            <div class="col-md-7"><?= $this->Form->input('icon', ['class'=>'form-control', 'onkeyup'=>'updateIcon()', 'id'=>'DeviceTypeIcon']); ?></div>
+            <div class="col-md-1"><i class="mdi mdi-<?= $device['icon'] ?>" style="font-size:24px" id="device_icon_preview"></i></div>
+          </div>
+          <div class="row mb-2">
+            <div class="col-sm-4">Attributes Allowed:</div>
+            <div class="col-sm-8"><?= $this->Form->select('attributes', $allowedAttributes, ['class'=>'custom-select','multiple'=>true,'value'=>$assignedAttributes]) ?><br />
+            These are attributes that are allowed to be recorded for this type. This does not affect if they are displayed.
+            </div>
+          </div>
+          <div class="row mb-2">
+            <div class="col-sm-4">Allow Decomissioning:</div>
+            <div class="col-sm-8"><?= $this->Form->select('allow_decom', ['false'=>'No', 'true'=>'Yes'], ['class'=>'custom-select','empty'=>false]) ?><br />
+            This controls if decomissioning is allowed for this device type.
+            </div>
+          </div>
+          <div class="row mb-2">
+            <div class="col-sm-4">Check Running Status:</div>
+            <div class="col-sm-8"><?= $this->Form->select('check_running', ['false'=>'No', 'true'=>'Yes'], ['class'=>'custom-select','empty'=>false]) ?><br />
+            On the device status page this will attempt to ping the device and allow other controls if enabled in the <?= $this->Html->link('settings', ['action'=>'settings']) ?>.
+            </div>
+          </div>
+          <div class="row mb-2">
+            <div class="col-sm-4">Exclude AD Compare</div>
+            <div class="col-sm-8"><?= $this->Form->select('exclude_ad_sync', ['false'=>'No', 'true'=>'Yes'], ['class'=>'custom-select','empty'=>false]) ?><br />
+            Will exclude this device type when doing Active Directory comparisons for missing devices.
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-8"><?= $this->Form->Submit('Update', ['class'=>'btn btn-primary btn-block']) ?></div>
+          </div>
+          <?= $this->Form->end(); ?>
+        </div>
+    </div>
+  </div>
+</div>
