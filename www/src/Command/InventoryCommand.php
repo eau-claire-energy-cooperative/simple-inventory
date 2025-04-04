e9a1a21985b5ac01e15a9a136a0d7b9049c0f725
Rob Weber <rweber@ecec.com>
2025-04-04 09:47:23 -0500

    load PHPmailer add sendMail as command function

--- www/src/Command/InventoryCommand.php
+++ www/src/Command/InventoryCommand.php
@@ -4,6 +4,8 @@ namespace App\Command;
 use Cake\Command\Command;
 use Cake\Console\Arguments;
 use Cake\Console\ConsoleIo;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 /*
 This class is not meant for actual use and should be extended by other CLI command classes
@@ -21,6 +23,48 @@ abstract class InventoryCommand extends Command
     $aLog->DATED = date("Y-m-d H:i:s",time());
 
     $Log->save($aLog);
+	}
+
+  public function sendMail($subject, $message, $recipient = ""){
+		//get the settings
+		$settings = $this->fetchTable('Setting')->find('list', ['keyField'=>'key', 'valueField'=>'value'])->toArray();
+
+		//setup the mailer
+		$email = new PHPMailer(true);
+		$email->IsSMTP();
+		$email->IsHTML(true);
+		$email->Host = $settings['smtp_server'];
+		$email->Port = 25;
+		$email->Username = $settings['smtp_user'];
+		$email->Password = $settings['smtp_pass'];
+		$email->SetFrom($settings['outgoing_email']);
+
+		//setup the subject/message
+		$email->Subject = $subject;
+		$email->Body = $message;
+
+    if(empty($recipient))
+    {
+  		//send to admin users
+  		$users = $this->fetchTable('User')->find('all', ['conditions'=>['User.send_email'=>'true']])->all();
+
+  		foreach($users as $aUser){
+  			//log email
+  			$this->dblog("Sending email to " . $aUser['User']['email']);
+
+  			$email->AddAddress($aUser['email']);
+  		}
+    }
+    else
+    {
+      //recipient is already set
+      $email->AddAddress($recipient);
+      $this->dblog("Sending email to " . $recipient);
+    }
+
+		//send the message
+		$email->Send();
+
 	}
 }
 ?>
