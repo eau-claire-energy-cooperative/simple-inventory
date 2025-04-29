c1d5852a065f23f3c6ff4b125a676c7e74ad276c
Rob Weber <rweber@ecec.com>
2025-04-29 13:08:07 -0500

    allow added auto grouping regex - part of #31

--- www/templates/Admin/edit_location.php
+++ www/templates/Admin/edit_location.php
@@ -9,9 +9,16 @@
           <h6 class="m-0 font-weight-bold text-primary"></h6>
         </div>
         <div class="card-body">
-          <div class="row">
+          <div class="row mb-1">
             <div class="col-md-4">Location Name: </div>
             <div class="col-md-8"><?= $this->Form->input('location', ['class'=>'form-control']);?></div>
+          </div>
+          <div class="row mb-1">
+            <div class="col-md-4">Auto Grouping Regex: </div>
+            <div class="col-md-8">
+              <?= $this->Form->input('auto_regex',['class'=>'form-control']); ?>
+              <a href="https://en.wikipedia.org/wiki/Regular_expression">Regular expression</a> that will put devices in this group automatically upon creation via the API based on the device name.
+            </div>
           </div>
           <div class="row mt-2">
             <div class="col-sm-4"></div>
