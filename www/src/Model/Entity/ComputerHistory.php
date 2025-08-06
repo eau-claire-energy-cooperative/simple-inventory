1fd142afaa3fd3943b83ed9e4e51d08b76d7ab7b
Rob Weber <rweber@ecec.com>
2025-08-06 14:47:12 -0500

    virtual fields to get json array

--- www/src/Model/Entity/ComputerHistory.php
+++ www/src/Model/Entity/ComputerHistory.php
@@ -5,8 +5,13 @@ use Cake\ORM\Entity;
 
 class ComputerHistory extends Entity{
 
-  protected function _getOrigJson($json){
-    return json_decode($json, true);
+  // virtual fields
+  protected function _getOrigAsJson(){
+    return json_decode($this->orig_json, true);
   }
+
+  protected function _getUpdatedAsJson(){
+  }
+    return json_decode($this->updated_json, true);
 }
 ?>
