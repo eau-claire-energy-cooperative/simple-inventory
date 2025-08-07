f9815a4508a812bd2546ea11308c40270c4195c5
Rob Weber <rweber@ecec.com>
2025-08-07 08:47:45 -0500

    added schema update for computer history

---
+++ config/inventory_schema_update_7.4.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `computer_history`;
+CREATE TABLE `computer_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `device_id` int(11) DEFAULT NULL,
+  `updated_timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `user` varchar(50) DEFAULT NULL,
+  `orig_json` text DEFAULT NULL,
+  `updated_json` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci
