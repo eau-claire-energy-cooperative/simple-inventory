589eef4fcfb9d6f07b27726f53a0ee1ffe16d2f3
Rob Weber <rweber@ecec.com>
2025-08-07 11:17:50 -0500

    log parsing can now use a modified markdown syntax for urls

--- www/src/View/Helper/LogParserHelper.php
+++ www/src/View/Helper/LogParserHelper.php
@@ -6,8 +6,41 @@ use Cake\View\Helper;
 class LogParserHelper extends Helper
 {
   public $helpers = ['Html'];
+  private $link_types = ["device"=> "/inventory/more-info/%d",
+                         "history"=> "/inventory/view-history/%d",
+                         "application"=> "/search/searchApplication/%d",
+                         "decom"=> "/inventory/more-info-decommissioned/%d",
+                         "device_type"=> "/search/search/5/%s"];
 
   function parseMessage($inventory, $message){
+
+    // match on the format [title](type:id)
+    preg_match_all('/\[(.*?)\]\((.*?):(.*?)\)/', $message, $matches);
+
+    // loop through every match and replace the text with the link
+    if(count($matches[0]) > 0)
+    {
+      foreach ($matches[0] as $i => $match) {
+
+        $link = $this->_createLink($matches[1][$i], $matches[2][$i], $matches[3][$i]);
+
+        $message = str_replace($match, $link, $message);
+      }
+    }
+    else
+    {
+      // if no matches try legacy parsing
+      $message = $this->_legacyParseMessage($inventory, $message);
+    }
+
+    return $message;
+  }
+
+  function _createLink($title, $type, $id){
+    return $this->Html->link($title, sprintf($this->link_types[$type], $id));
+  }
+
+  function _legacyParseMessage($inventory, $message){
 
     //try and find a device name in this string
     foreach(array_keys($inventory) as $aKey)
