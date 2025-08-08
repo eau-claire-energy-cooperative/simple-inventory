ee6c5d1cb48d23aa7f7dc5233629e6d17a04cc39
Rob Weber <rweber@ecec.com>
2025-08-08 13:29:28 -0500

    make logs table look like other table pages added paginated filter

--- www/config/paginator-templates.php
+++ www/config/paginator-templates.php
@@ -1,8 +1,10 @@
 <?php
 return [
-  'prevActive' => '<a href="{{url}}">{{text}}</a>',
-  'nextActive' => '<a href="{{url}}">{{text}}</a>',
-  'prevDisabled' => '{{text}}',
-  'nextDisabled' => '{{text}}',
+  'prevActive' => '<li class="dt-paging-button page-item"><a href="{{url}}" class="page-link">{{text}}</a></li>',
+  'nextActive' => '<li class="dt-paging-button page-item"><a href="{{url}}" class="page-link">{{text}}</a></li>',
+  'prevDisabled' => '<li class="dt-paging-button page-item disabled"><a class="page-link previous">{{text}}</a></li>',
+  'nextDisabled' => '<li class="dt-paging-button page-item disabled"><a class="page-link next">{{text}}</a></li>',
+  'number'=> '<li class="dt-paging-button page-item"><a href="{{url}}" class="page-link">{{text}}</a></li>',
+  'current' => '<li class="dt-paging-button page-item active"><a href="{{url}}" class="page-link">{{text}}</a></li>'
 ];
 ?>
--- www/templates/Admin/logs.php
+++ www/templates/Admin/logs.php
@@ -1,21 +1,59 @@
-
+<?= $this->Html->css('dataTables.bootstrap4.min', ['block'=>'css']) ?>
 <div class="row">
-  <div class="col-md-4"><?= $this->Paginator->prev("<< Newer "); ?></div>
-	<div class="col-md-4"><p align="center"><?= $this->Paginator->counter('Displaying page {{page}} of {{pages}}') ?></p></div>
-	<div class="col-md-4"><p align="right"><?= $this->Paginator->next("Older >> ") ?></p></div>
-</div>
-<table class="table table-striped">
-  <tbody>
-    <?php foreach ($logs as $post): ?>
-    <tr>
-        <td> <?=  $post['id']; ?></td>
-        <td> <?=  $this->Time->nice( $post['DATED']) ; ?></td>
-        <td> <?=  $post['LOGGER']; ?></td>
-        <td> <?=  $post['LEVEL']; ?></td>
-        <td> <?= $post['USER'] ?></td>
-        <td> <?=  $this->LogParser->parseMessage($inventory,$post['MESSAGE']); ?></td>
-    </tr>
+  <div class="col-xl-12">
+    <div class="card shadow mb-4">
+      <div class="card-header py-3">
 
-    <?php endforeach; ?>
-  </tbody>
-</table>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-8"><?= $this->Paginator->counter('Displaying page {{page}} of {{pages}}') ?></div>
+      	  <div class="col-md-4">
+            <?= $this->Form->create(null, ['url'=>'/admin/logs']) ?>
+            <div class="form-group row">
+              <label for="fQuery" class="col-sm-4 col-form-label text-right">Filter: </label>
+              <div class="col-sm-8">
+                <?= $this->Form->text('q', ['id'=>'fQuery', 'class'=>'form-control']) ?>
+              </div>
+            </div>
+            <?= $this->Form->end() ?>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="col-md-8">
+            <div style="float:right">
+              <ul class="pagination">
+                <?= $this->Paginator->prev("Previous"); ?>
+                <?= $this->Paginator->numbers(['before'=>'', 'modulus'=>3]) ?>
+                <?= $this->Paginator->next("Next") ?>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <table class="table table-striped">
+          <thead>
+            <th>ID</th>
+            <th>Date</th>
+            <th>Area</th>
+            <th>Level</th>
+            <th>User</th>
+            <th>Message</th>
+          </thead>
+          <tbody>
+            <?php foreach ($logs as $post): ?>
+            <tr>
+                <td><?= $post['id'] ?></td>
+                <td><?= $this->Time->nice( $post['DATED']) ?></td>
+                <td><?= $post['LOGGER'] ?></td>
+                <td><?= $post['LEVEL'] ?></td>
+                <td><?= $post['USER'] ?></td>
+                <td><?= $this->LogParser->parseMessage($inventory,$post['MESSAGE']) ?></td>
+            </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
